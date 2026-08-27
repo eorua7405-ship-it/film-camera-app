868e768e80461158e1266b58eda8a70485f3e36e
eorua7405-ship-it <eorua7405@gmail.com>
2026-08-27 21:02:02 +0900

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.filmcam.app',
-  appName: '필름 카메라',
-  webDir: 'www'
-};
-
-export default config;
